81cf3a5fbf63a0c554b909f6b329f205e0eff1aa
Mattdeall <mehmetaliborekci@gmail.com>
2025-03-03 23:55:10 +0300

    doWhile Loop added

--- js03/js03-loops/3-doWhileLoop.js
+++ js03/js03-loops/3-doWhileLoop.js
@@ -3,3 +3,15 @@
 // * ============================================
 
 console.log("****** DO-WHILE *******");
+
+let devammi="";
+
+do{
+    let vize= +prompt("vize");
+    let final= +prompt("final");
+    //ortalama
+    console.log((vize + final) / 2)
+
+    devammi = prompt("devam?")
+}while(devammi == "evet");
+console.log("Programı kullandığınız için teşekkürler");
